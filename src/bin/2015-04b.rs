2a9f3e4ef3898a2466a0e8cae5ecfd067e727a09
Michael P. Jung <michael.jung@terreon.de>
2020-08-22 10:33:17 +0200

    Add solution for 2015-04

---
+++ src/bin/2015-04b.rs
@@ -0,0 +1,18 @@
+const INPUT: &str = include_str!("2015-04.txt");
+
+fn main() {
+    let secret = INPUT.trim();
+    let output = (1..)
+        .filter_map(|i| {
+            let s = format!("{}{}", secret, i);
+            let digest = md5::compute(s);
+            if format!("{:?}", digest).starts_with("000000") {
+                Some(i)
+            } else {
+                None
+            }
+        })
+        .next()
+        .unwrap();
+    println!("{:?}", output);
+}
