df8860a0427b34dc35618fcdd462dd9da6f088a8
Michael P. Jung <michael.jung@terreon.de>
2020-08-25 12:28:33 +0200

    Add solution for 2015-10

---
+++ src/bin/2015-10b.rs
@@ -0,0 +1,14 @@
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("2015-10.txt");
+
+fn main() {
+    let output = (0..50).fold(INPUT.trim().to_owned(), |s, _| {
+        s.chars()
+            .group_by(|&c| c)
+            .into_iter()
+            .map(|(k, g)| format!("{}{}", g.count(), k))
+            .join("")
+    });
+    println!("{}", output.len());
+}
