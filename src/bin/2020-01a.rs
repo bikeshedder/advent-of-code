07659a4b2e083ed17a65f60018e380703ff8448c
Michael P. Jung <michael.jung@terreon.de>
2020-12-01 16:12:36 +0100

    Add solution for 2020-01

---
+++ src/bin/2020-01a.rs
@@ -0,0 +1,16 @@
+const INPUT: &str = include_str!("2020-01.txt");
+
+fn main() {
+    let mut numbers = INPUT
+        .trim()
+        .split_whitespace()
+        .map(|line| line.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+    numbers.sort();
+    for (i, n) in itertools::enumerate(&numbers) {
+        match numbers[i..].binary_search(&(2020 - n)) {
+            Ok(j) => { println!("{}", (n * numbers[i+j])); }
+            Err(_) => {}
+        }
+    }
+}
---
+++ src/bin/2020-01b.rs
@@ -0,0 +1,18 @@
+const INPUT: &str = include_str!("2020-01.txt");
+
+fn main() {
+    let mut numbers = INPUT
+        .trim()
+        .split_whitespace()
+        .map(|line| line.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+    numbers.sort();
+    for (i, n) in itertools::enumerate(&numbers) {
+        for (j, m) in itertools::enumerate(&numbers[i..]) {
+            match numbers[i+j..].binary_search(&(2020 - n - m)) {
+                Ok(k) => { println!("{}", (n * m * numbers[i+j+k])); }
+                Err(_) => {}
+            }
+        }
+    }
+}
