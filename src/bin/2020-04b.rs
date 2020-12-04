e0534451d6fa71c8253f959c4dea0942086ac04c
Michael P. Jung <michael.jung@terreon.de>
2020-12-04 14:09:04 +0100

    Clean up solution for 2020-04 part 2

--- src/bin/2020-04b.rs
+++ src/bin/2020-04b.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use scan_fmt::scan_fmt;
 
 const INPUT: &str = include_str!("2020-04.txt");
 const EYE_COLORS: [&str; 7] = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
@@ -9,14 +10,22 @@ fn check_year(s: &str, min: u64, max: u64) -> bool {
         .unwrap_or(false)
 }
 
+fn check_height(s: &str) -> bool {
+    scan_fmt!(s, "{d}{}", u64, String)
+        .and_then(|(x, u)| match (x, u.as_str()) {
+            (x, "cm") => Ok(150 <= x && x <= 193),
+            (x, "in") => Ok(59 <= x && x <= 76),
+            _ => Ok(false),
+        })
+        .unwrap_or(false)
+}
+
 fn main() {
-    let hgt_re = Regex::new(r"^(\d+)(in|cm)$").unwrap();
     let hcl_re = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
     let pid_re = Regex::new(r"^\d{9}$").unwrap();
     let solution = INPUT
-        .trim()
         .split("\n\n")
-        .map(|p| {
+        .filter(|p| {
             p.split_whitespace()
                 .filter_map(|f| {
                     let mut split = f.splitn(2, ":");
@@ -26,18 +35,7 @@ fn main() {
                     "byr" => check_year(v, 1920, 2002),
                     "iyr" => check_year(v, 2010, 2020),
                     "eyr" => check_year(v, 2020, 2030),
-                    "hgt" => hgt_re
-                        .captures(v)
-                        .and_then(|c| {
-                            let x = c[1].parse::<u64>().ok()?;
-                            let u = &c[2];
-                            match (x, u) {
-                                (x, "cm") => Some(150 <= x && x <= 193),
-                                (x, "in") => Some(59 <= x && x <= 76),
-                                _ => None,
-                            }
-                        })
-                        .unwrap_or(false),
+                    "hgt" => check_height(v),
                     "hcl" => hcl_re.find(v).is_some(),
                     "ecl" => EYE_COLORS.contains(&v),
                     "pid" => pid_re.find(v).is_some(),
@@ -45,8 +43,8 @@ fn main() {
                 })
                 .filter(|&ok| ok)
                 .count()
+                >= 7
         })
-        .filter(|&c| c >= 7)
         .count();
     println!("{:?}", solution);
 }
