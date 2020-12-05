8da5294badd87e4cdec7800c53363b2710158eb7
Michael P. Jung <michael.jung@terreon.de>
2020-12-05 19:56:56 +0100

    Fix edge cases of solutions for 2020-05

--- 2020/src/bin/05a.rs
+++ 2020/src/bin/05a.rs
@@ -5,12 +5,12 @@ const INPUT: &str = include_str!("../input/05.txt");
 fn main() {
     let solution = INPUT
         .lines()
-        .sorted()
-        .next()
         .map(|line| {
             line.chars()
                 .fold(0, |n, d| (n << 1) + (d == 'B' || d == 'R') as u64)
         })
+        .sorted()
+        .last()
         .unwrap();
     println!("{}", solution);
 }
--- 2020/src/bin/05b.rs
+++ 2020/src/bin/05b.rs
@@ -5,11 +5,11 @@ const INPUT: &str = include_str!("../input/05.txt");
 fn main() {
     let solution = INPUT
         .lines()
-        .sorted()
         .map(|line| {
             line.chars()
                 .fold(0, |n, d| (n << 1) + (d == 'B' || d == 'R') as u64)
         })
+        .sorted()
         .tuple_windows()
         .find(|(a, b)| *a + 2 == *b)
         .unwrap()
