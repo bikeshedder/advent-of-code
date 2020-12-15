a1b35ce2a43ab81864dc4bf8307f21c901be392e
Michael P. Jung <michael.jung@terreon.de>
2020-12-15 16:55:48 +0100

    Refactor and optimize solution for 2020-15

--- 2020/src/bin/15.rs
+++ 2020/src/bin/15.rs
@@ -1,30 +1,27 @@
-use std::collections::{hash_map::Entry, HashMap};
-
 const INPUT: &str = include_str!("../input/15.txt");
 
-fn step(past: &mut HashMap<usize, usize>, n: usize, i: usize) -> usize {
-    match past.entry(n) {
-        Entry::Vacant(entry) => {
-            entry.insert(i);
-            0
-        }
-        Entry::Occupied(mut entry) => i - entry.insert(i),
-    }
+fn step(history: &mut [u32], n: u32, i: u32) -> u32 {
+    let next = match history[n as usize] {
+        std::u32::MAX => 0,
+        j => i - j
+    };
+    history[n as usize] = i;
+    next
 }
 
-fn game(nth: usize) -> usize {
-    let numbers: Vec<usize> = INPUT
+fn game(nth: u32) -> u32 {
+    let start: Vec<u32> = INPUT
         .trim()
         .split(",")
         .filter_map(|n| n.parse().ok())
         .collect();
-    let mut past: HashMap<usize, usize> = HashMap::new();
-    let mut next = 0usize;
-    for (i, n) in numbers.iter().enumerate() {
-        next = step(&mut past, *n, i);
+    let mut history = vec![std::u32::MAX; nth as usize];
+    let mut next = 0u32;
+    for (n, i) in start.iter().zip(0u32..) {
+        next = step(&mut history, *n, i);
     }
-    for i in numbers.len()..nth - 1 {
-        next = step(&mut past, next, i);
+    for i in start.len() as u32..nth - 1 {
+        next = step(&mut history, next, i);
     }
     next
 }
