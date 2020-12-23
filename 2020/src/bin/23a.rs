e1f2b4cba3b8842a6ee216689034ffc3488ee73d
Michael P. Jung <michael.jung@terreon.de>
2020-12-23 14:09:23 +0100

    Clean up solution for 23 part 1

--- 2020/src/bin/23a.rs
+++ 2020/src/bin/23a.rs
@@ -5,20 +5,23 @@ const LEN: usize = INPUT.len();
 const MOVES: usize = 100;
 
 fn main() {
+    // parse input
     let mut cups: Vec<i8> = INPUT
         .chars()
         .map(|c| c.to_digit(10).unwrap() as i8)
         .collect();
+    // play the game
     for _ in 0..MOVES {
-        let cup = cups[0];
         let insert_idx = (4..LEN)
-            .min_by_key(|i| (cup - cups[*i]).rem_euclid(LEN as i8))
+            .min_by_key(|i| (cups[0] - cups[*i]).rem_euclid(LEN as i8))
             .unwrap();
         cups[1..insert_idx + 1].rotate_left(3);
         cups.rotate_left(1);
     }
+    // move cup 1 to the front
     let cup1_idx = cups.iter().position(|cup| *cup == 1).unwrap();
     cups.rotate_left(cup1_idx as usize);
+    // output answer
     let answer = cups[1..]
         .iter()
         .map(|d| char::from_digit(*d as u32, 10).unwrap())
