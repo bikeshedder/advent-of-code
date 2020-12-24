1cf242754d21c69d7d46b7f30245a9c97fbe2993
Michael P. Jung <michael.jung@terreon.de>
2020-12-24 13:51:55 +0100

    Clean up solution for 2020-24

--- 2020/src/bin/24.rs
+++ 2020/src/bin/24.rs
@@ -1,99 +1,96 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, hash::Hash, ops::Add};
 
 use regex::Regex;
 
-type Coord = (i32, i32);
-
 const INPUT: &str = include_str!("../input/24.txt");
+const NEIGHBORS: [Coord; 6] = [
+    Coord(1, 0),   // e
+    Coord(0, -1),  // se
+    Coord(-1, -1), // sw
+    Coord(-1, 0),  // w
+    Coord(0, 1),   // nw
+    Coord(1, 1),   // ne
+];
 
-fn get_neighbors(c: Coord) -> [Coord; 6] {
-    [
-        (c.0 + 1, c.1),     // e
-        (c.0, c.1 - 1),     // se
-        (c.0 - 1, c.1 - 1), // sw
-        (c.0 - 1, c.1),     // w
-        (c.0, c.1 + 1),     // nw
-        (c.0 + 1, c.1 + 1), // ne
-    ]
-}
+#[derive(Copy, Clone, Hash, Eq, PartialEq)]
+struct Coord(i32, i32);
 
-fn get_neighbor(c: Coord, dir: &str) -> Coord {
-    match dir {
-        "e" => (c.0 + 1, c.1),
-        "se" => (c.0, c.1 - 1),
-        "sw" => (c.0 - 1, c.1 - 1),
-        "w" => (c.0 - 1, c.1),
-        "nw" => (c.0, c.1 + 1),
-        "ne" => (c.0 + 1, c.1 + 1),
-        _ => unreachable!(),
+impl Add for Coord {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Coord(self.0 + rhs.0, self.1 + rhs.1)
     }
 }
 
-fn follow_path(c: Coord, path: &[&str]) -> Coord {
-    path.iter().fold(c, |c, dir| get_neighbor(c, dir))
-}
-
-#[derive(Clone, Default)]
-struct Floor(HashSet<Coord>);
-
-impl Floor {
-    fn flip(&mut self, c: Coord) {
-        if !self.0.remove(&c) {
-            self.0.insert(c);
+impl Coord {
+    fn from_dir(s: &str) -> Coord {
+        match s {
+            "e" => NEIGHBORS[0],
+            "se" => NEIGHBORS[1],
+            "sw" => NEIGHBORS[2],
+            "w" => NEIGHBORS[3],
+            "nw" => NEIGHBORS[4],
+            "ne" => NEIGHBORS[5],
+            _ => unreachable!(),
         }
     }
-    fn flip_to_white(&mut self, c: Coord) {
-        self.0.remove(&c);
-    }
-    fn flip_to_black(&mut self, c: Coord) {
-        self.0.insert(c);
-    }
-    fn black_tiles(&self) -> usize {
-        self.0.len()
+    fn follow_path(&self, path: &[Coord]) -> Coord {
+        path.iter().fold(*self, |c, dir| c + *dir)
     }
 }
+
+type Floor = HashSet<Coord>;
 
 fn main() {
     let re = Regex::new("e|se|sw|w|nw|ne").unwrap();
     let paths = INPUT
         .lines()
-        .map(|line| re.find_iter(line).map(|c| c.as_str()).collect::<Vec<_>>())
+        .map(|line| {
+            re.find_iter(line)
+                .map(|c| Coord::from_dir(c.as_str()))
+                .collect::<Vec<_>>()
+        })
         .collect::<Vec<_>>();
     // part 1
-    let mut floor = Floor::default();
+    let mut floor = Floor::new();
+    let center = Coord(0, 0);
     for path in paths {
-        let c = follow_path((0, 0), &path);
-        floor.flip(c);
+        let c = center.follow_path(&path);
+        if !floor.remove(&c) {
+            floor.insert(c);
+        }
     }
-    println!("{}", floor.black_tiles());
+    println!("{}", floor.len());
     // part 2
     for _ in 1..=100 {
-        let black_tiles = floor.0.clone();
-        let mut white_tiles = HashSet::<Coord>::new();
+        let black_tiles = floor.clone();
+        let mut white_tiles = Floor::new();
         for &c in black_tiles.iter() {
             let mut black_neighbors = 0;
-            for nc in get_neighbors(c).iter() {
-                if black_tiles.contains(nc) {
+            for &n in NEIGHBORS.iter() {
+                let nc = c + n;
+                if black_tiles.contains(&nc) {
                     black_neighbors += 1;
                 } else {
-                    white_tiles.insert(*nc);
+                    white_tiles.insert(nc);
                 }
             }
             if black_neighbors == 0 || black_neighbors > 2 {
-                floor.flip_to_white(c);
+                floor.remove(&c);
             }
         }
         for &c in white_tiles.iter() {
             let mut black_neighbors = 0;
-            for nc in get_neighbors(c).iter() {
-                if black_tiles.contains(nc) {
+            for &n in NEIGHBORS.iter() {
+                let nc = c + n;
+                if black_tiles.contains(&nc) {
                     black_neighbors += 1;
                 }
             }
             if black_neighbors == 2 {
-                floor.flip_to_black(c);
+                floor.insert(c);
             }
         }
     }
-    println!("{}", floor.black_tiles());
+    println!("{}", floor.len());
 }
