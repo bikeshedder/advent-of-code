4586df00f9290e4859098ea61ba9a027f4effbee
Michael P. Jung <michael.jung@terreon.de>
2021-12-01 23:09:37 +0100

    Clean up solution for 2021-01

--- 2021/src/bin/01a.rs
+++ 2021/src/bin/01a.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 const INPUT: &str = include_str!("../input/01.txt");
 
 fn main() {
@@ -7,9 +9,9 @@ fn main() {
         .map(|line| line.parse::<i64>().unwrap())
         .collect::<Vec<_>>();
     let answer = numbers
-        .windows(2)
-        .map(|w| w.get(1).unwrap() > w.get(0).unwrap())
-        .filter(|b| *b)
+        .iter()
+        .tuple_windows()
+        .filter(|(a, b)| a < b)
         .count();
     println!("{}", answer);
 }
--- 2021/src/bin/01b.rs
+++ 2021/src/bin/01b.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 const INPUT: &str = include_str!("../input/01.txt");
 
 fn main() {
@@ -6,14 +8,12 @@ fn main() {
         .split_whitespace()
         .map(|line| line.parse::<i64>().unwrap())
         .collect::<Vec<_>>();
-    let sums = numbers
-        .windows(3)
-        .map(|w| w.iter().sum::<i64>())
-        .collect::<Vec<_>>();
-    let answer = sums
-        .windows(2)
-        .map(|w| w.get(1).unwrap() > w.get(0).unwrap())
-        .filter(|b| *b)
+    let answer = numbers
+        .iter()
+        .tuple_windows()
+        .map(|(a, b, c)| a + b + c)
+        .tuple_windows()
+        .filter(|(a, b)| a < b)
         .count();
     println!("{}", answer);
 }
