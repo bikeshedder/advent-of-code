d58f2005d190d80dc58de20b096f70d7abba27d4
Michael P. Jung <michael.jung@terreon.de>
2021-12-13 14:57:00 +0100

    Add solution for 2021-06

---
+++ 2021/src/bin/06a.rs
@@ -0,0 +1,23 @@
+const INPUT: &str = include_str!("../input/06.txt");
+
+fn main() {
+    let fishes = INPUT
+        .trim()
+        .split(',')
+        .map(|line| line.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    let mut timers = [0; 9];
+    for fish in fishes {
+        timers[fish] += 1;
+    }
+    for _ in 0..80 {
+        let creating = timers[0];
+        for i in 0..8 {
+            timers[i] = timers[i + 1];
+        }
+        timers[6] += creating;
+        timers[8] = creating;
+    }
+    let answer: usize = timers.iter().sum();
+    println!("{}", answer);
+}
